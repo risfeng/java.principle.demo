7da75a8905215feda526f7e8a162e777934587d3
xiaofeng <xiaofeng@gaodun.com>
2019-12-15 14:00:38 +0800

    use thread pool .

--- thread-collaboration/src/main/java/com/adotcode/threadcollaboration/ThreadCollaborationApplication.java
+++ thread-collaboration/src/main/java/com/adotcode/threadcollaboration/ThreadCollaborationApplication.java
@@ -2,6 +2,11 @@ package com.adotcode.threadcollaboration;
 
 import com.adotcode.threadcollaboration.thread.LetterRunnable;
 import com.adotcode.threadcollaboration.thread.NumberRunnable;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author risfeng
@@ -11,9 +16,17 @@ public class ThreadCollaborationApplication {
   private static Object object = new Object();
 
   public static void main(String[] args) {
+    ExecutorService executorService = new ThreadPoolExecutor(
+        2,
+        2,
+        1000,
+        TimeUnit.MILLISECONDS,
+        new ArrayBlockingQueue<>(5),
+        Executors.defaultThreadFactory(),
+        (r, executor) -> System.out.println(r.toString() + "执行了拒绝策略"));
     NumberRunnable numberRunnable = new NumberRunnable(object);
     LetterRunnable letterRunnable = new LetterRunnable(object);
-    new Thread(numberRunnable).start();
-    new Thread(letterRunnable).start();
+    executorService.execute(numberRunnable);
+    executorService.execute(letterRunnable);
   }
 }
