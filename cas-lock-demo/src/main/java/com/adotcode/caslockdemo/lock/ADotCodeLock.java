dbc8ce22ab6ddba82035c85ce2f27c7b77b6a0b3
xiaofeng <xiaofeng@gaodun.com>
2020-01-16 16:10:55 +0800

    test cas.

--- cas-lock-demo/src/main/java/com/adotcode/caslockdemo/lock/ADotCodeLock.java
+++ cas-lock-demo/src/main/java/com/adotcode/caslockdemo/lock/ADotCodeLock.java
@@ -1,5 +1,9 @@
 package com.adotcode.caslockdemo.lock;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.objenesis.instantiator.util.UnsafeUtils;
+import sun.misc.Unsafe;
+
 /**
  * Adc锁
  *
@@ -8,4 +12,48 @@ package com.adotcode.caslockdemo.lock;
  */
 public class ADotCodeLock {
 
+  private volatile int state = 0;
+  private static final Unsafe unsafe = UnsafeUtils.getUnsafe();
+  private static long valueOffset;
+  private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+  static {
+    try {
+      valueOffset = unsafe.objectFieldOffset(ADotCodeLock.class.getDeclaredField("state"));
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void lock() throws NoSuchFieldException {
+    System.out.println("原始值：state=" + state + " valueOffset=" + valueOffset);
+
+    System.out.println(unsafe.compareAndSwapInt(this, 24, 0, 1));
+    System.out.println("修改后的值：state=" + state + " valueOffset=" + valueOffset);
+
+    System.out.println(unsafe.compareAndSwapInt(this, 48, 1, 2));
+    valueOffset = unsafe.objectFieldOffset(ADotCodeLock.class.getDeclaredField("state"));
+
+    System.out.println("再修改后的值：state=" + state + " valueOffset=" + valueOffset);
+
+  }
+
+  private void atomicTest() {
+    System.out.println("原始值：atomicInteger=" + atomicInteger.get());
+
+    System.out.println(atomicInteger.compareAndSet(1, 2));
+    System.out.println("没有更新成功的值：atomicInteger=" + atomicInteger.get());
+
+    System.out.println(atomicInteger.compareAndSet(0, 2));
+    System.out.println("更新成功的值：atomicInteger=" + atomicInteger.get());
+  }
+
+  public static void main(String[] args) {
+    ADotCodeLock aDotCodeLock = new ADotCodeLock();
+    try {
+      aDotCodeLock.atomicTest();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
---
+++ thread-collaboration/src/main/java/com/adotcode/threadcollaboration/UseThreadApplication.java
@@ -0,0 +1,14 @@
+package com.adotcode.threadcollaboration;
+
+/**
+ * 几种线程的使用方式
+ *
+ * @author risfeng
+ * @date 2020/01/14
+ */
+public class UseThreadApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
